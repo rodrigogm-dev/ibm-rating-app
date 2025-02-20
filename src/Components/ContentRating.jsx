409f9f410d2cd41bbb8f38a6db3e90d96b78ed15
rodrigo gayoso <rodrigogm.dev@gmail.com>
2025-02-20 14:04:11 -0500

    added totalRating feature

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -8,15 +8,18 @@ class ContentRating extends Component {
         this.state = {
             likes: 0,
             dislikes: 0,
+            totalRatings: 0,
             handleLike:() => {
                 this.setState((prevState) => ({
-                likes: prevState.likes + 1
+                likes: prevState.likes + 1,
+                totalRatings: prevState.totalRatings + 1
                 }));
             },
 
             handleDislike:() => {
                 this.setState((prevState) => ({
-                dislikes: prevState.dislikes + 1
+                dislikes: prevState.dislikes + 1,
+                totalRatings: prevState.totalRatings + 1
                 }));
             }
         }
@@ -36,6 +39,9 @@ class ContentRating extends Component {
                     <button className="dislike-button" onClick={this.state.handleDislike}>
                         Dislike ({this.state.dislikes})
                     </button>
+                    <p>
+                        Total Ratings: {this.state.totalRatings}
+                    </p>
                 </div>
             </div>
         </>
